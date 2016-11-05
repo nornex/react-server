7b645d793ad2b291f75b0bcebe75397dc9c428e1
Matthew H <nornex@gmail.com>
2016-11-05 13:58:44 +0000

    Add bootstrap check for windows symlinks (mklink).

--- packages/react-server-test-pages/gulpfile.babel.js
+++ packages/react-server-test-pages/gulpfile.babel.js
@@ -2,6 +2,9 @@ const eslint = require("gulp-eslint");
 const gulp = require("gulp");
 const nsp = require("gulp-nsp");
 const path = require("path");
+const process = require("process");
+
+const linebreaks = process.platform.startsWith('win') ? 'windows' : 'unix';
 
 const SRC = ["**/*.js", "!node_modules/**", "!__clientTemp/**"];
 
@@ -12,7 +15,7 @@ gulp.task("build", ["eslint", "nsp"]);
 gulp.task('nsp', cb => nsp({package: path.resolve('package.json')}, cb));
 
 gulp.task("eslint", [], () => gulp.src(SRC)
-	.pipe(eslint())
+	.pipe(eslint({"rules":{"linebreak-style": ['error', linebreaks]}}))
 	.pipe(eslint.format())
 	.pipe(eslint.failAfterError())
 );
